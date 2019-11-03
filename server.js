4b6f227a458e21fe71b5b0bc9c4446c1e9144c0e
B Kelly <berenicekelly808@gmail.com>
2019-11-02 23:48:46 -0700

    server.js done

---
+++ server.js
@@ -0,0 +1,26 @@
+//Require express
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+// Listen for client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
